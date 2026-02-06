f48b0489aa8b2b50e7a23df4c73947a7d6b1e42f
Chetna Ambe <chetnaambe75@gmail.com>
2026-02-06 23:25:34 +0530

    Web page

---
+++ Webpage1/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+const Nav= ()=>{
+    return(
+        <div id="nav">
+            <div id="left">
+                <a href="#">Home</a>
+                <a href="#">Profile</a>
+                <a href="#">Character</a>
+                <a href="#">Game</a>
+            </div>
+            <div id="right">
+                <button id="one">Search</button>
+                <button id="two">For More Info</button>
+            </div>
+        </div>
+    )
+}
+export default Nav;
